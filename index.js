63868aa7c115ff73601ca079532f7370547f24ab
lbravo <luis.bravo@safertaxi.com>
2014-06-21 16:07:07 -0300

    test

--- index.js
+++ index.js
@@ -64,6 +64,51 @@ app.use(allowCrossDomain);
 var router = express.Router();
 
 router.use(express.static(__dirname + '/public'));
+router.get('/test', function(rqe, res){
+    var bug = {
+        "id": 1,
+        "application_id": 9,
+        "environment_id": 2,
+        "country": "AR",
+        "language": "SPA",
+        "os": "Windows",
+        "os_version": "7",
+        "application_version": null,
+        "title": "No puedo entrar.",
+        "description": "No puedo entrar a la aplicacion.",
+        "steps": null,
+        "booking_id": null,
+        "user_id": null,
+        "driver_id": null,
+        "browser": "chrome",
+        "browser_version": "x",
+        "created": null,
+        "Environment": {
+            "id": 2,
+            "name": "Testing"
+        },
+        "Files": [
+            {
+                "id": 1,
+                "name": "files/4105-1icqtg5.png",
+                "bug_id": 1
+            },
+            {
+                "id": 2,
+                "name": "files/4335-1n5jm6m.jpg",
+                "bug_id": 1
+            }
+        ],
+        "Application": {
+            "id": 9,
+            "name": "Corporate Portal",
+            "type": "web"
+        }
+    };
+
+    res.send(bug);
+});
+
 router.use(auth);
 router.use(function(req, res, next){
 
